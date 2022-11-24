0bf53d2e27c779b1dde2f8b7c34e636c59afae85
greit1 <greit@gridcoding.com>
2022-11-24 10:55:03 +0100

    Refactor translation javascript file

--- resources/js/fustanella-scripts/translation/index.js
+++ resources/js/fustanella-scripts/translation/index.js
@@ -7,16 +7,29 @@ let server_pathname = window.location.pathname;
 
 var drop_down_menu = document.getElementById('dropdown-language');
 
-var current_language_src = document.getElementById('current-lang');
+var current_language = document.getElementById('current-lang');
 
 var other_language_slug = document.getElementById('other-lang-slug');
-var other_language_image = document.getElementById('other-lang-image');
+var other_language_text = document.getElementById('other-lang-image');
 
+
+// Mobile Menu
+
+var toggle_lang_dropdown_mobile = document.getElementById("select-lang1");
+
+var drop_down_menu_mobile = document.getElementById('dropdown-language1');
+
+var current_language_mobile = document.getElementById('current-lang1');
+
+var other_language_mobile_slug = document.getElementById('other-lang-slug1');
+var other_language_mobile_text = document.getElementById('other-lang-image1');
 
 
 if (sq_path) {
 
     // Manipulate DOM 
+
+    let english_url = window.location.pathname.replace('/sq/', "/");
 
     // let url_origin = window.location.origin;
 
@@ -25,53 +38,28 @@ if (sq_path) {
     // current_language_src.src = url_origin + '/fustanella/wp-content/themes/fustanella/resources/images/language-icons/al.svg';
     // other_language_image.src = url_origin + '/fustanella/wp-content/themes/fustanella/resources/images/language-icons/en.svg';
 
-    current_language_src.innerText = 'AL';
+    current_language.innerText = 'AL';
+    
     // if user wants to go to english 
-    let english_url = window.location.pathname.replace('/sq/', "/");
+    
     other_language_slug.setAttribute('href', english_url);
-    other_language_image.innerText = 'EN';
+    other_language_text.innerText = 'EN';
+    
+    // Mobile
+
+    current_language_mobile.innerText = 'AL';
+    other_language_mobile_slug.setAttribute('href', english_url);
+    other_language_mobile_text.innerText = 'EN';
 }
 
+//Desktop toggle
 
 toggle_lang_dropdown.addEventListener('click', () => {
     drop_down_menu.classList.remove('hidden');
 })
-// Fix urls when change language for mobile
 
-var toggle_lang_dropdown1 = document.getElementById("select-lang1");
-// Get url 
-var sq_path1 = window.location.pathname.includes('sq');
-let server_pathname1 = window.location.pathname;
+//Mobile toggle
 
-var drop_down_menu1 = document.getElementById('dropdown-language1');
-
-var current_language_src1 = document.getElementById('current-lang1');
-
-var other_language_slug1 = document.getElementById('other-lang-slug1');
-var other_language_image1 = document.getElementById('other-lang-image1');
-
-
-
-
-if (sq_path1) {
-
-    // Manipulate DOM 
-
-    let url_origin1 = window.location.origin;
-
-    // Swap Icons when url change (flags)
-    // if url origin doesnt contain fustanella, remove it 
-    // current_language_src1.src = url_origin1 + '/fustanella/wp-content/themes/fustanella/resources/images/language-icons/al.svg';
-    // other_language_image1.src = url_origin1 + '/fustanella/wp-content/themes/fustanella/resources/images/language-icons/en.svg';
-
-    current_language_src1.innerText = 'AL';
-    // if user wants to go to english 
-    let english_url1 = window.location.pathname.replace('/sq/', "/");
-    other_language_slug1.setAttribute('href', english_url1);
-    other_language_slug1.innerText = 'EN';
-}
-
-
-toggle_lang_dropdown1.addEventListener('click', () => {
-    drop_down_menu1.classList.remove('hidden');
+toggle_lang_dropdown_mobile.addEventListener('click', () => {
+    drop_down_menu_mobile.classList.remove('hidden');
 })
