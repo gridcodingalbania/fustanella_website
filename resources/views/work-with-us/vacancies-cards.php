550ad9b31f0ce1021987f3ecdcaed45a944d936a
greit1 <greit@gridcoding.com>
2022-12-02 13:27:31 +0100

    fixes

--- resources/views/work-with-us/vacancies-cards.php
+++ resources/views/work-with-us/vacancies-cards.php
@@ -8,9 +8,9 @@
         ?>
 
             <div class="bg-[#F0F0F0] card-shadows flex flex-col items-center justify-around xs:w-[154px] sm:w-[180px] md:w-[300px] xl:w-[300px] 2xl:w-[384px] xs:h-[148px] sm:h-[148px] md:h-[200px] xl:h-[300px] 2xl:h-[384px] ">
-                <p class="cormorant uppercase xs:text-[14px] sm:text-[14px] lg:text-[24px]"><?php the_sub_field('opened_position'); ?></p>
+                <p class="cormorant font-[500] uppercase xs:text-[14px] sm:text-[14px] lg:text-[24px]"><?php the_sub_field('opened_position'); ?></p>
                 <?php if ($image) : ?>
-                    <img src="<?php echo $picture ?>" class="xs:w-[38px] sm:w-[38px] lg:w-[52px] 2xl:w-[84px]">
+                    <img src="<?php echo $picture ?>" class="xs:w-[38px] sm:w-[38px] lg:w-[52px] 2xl:w-auto">
                 <?php endif; ?>
                 <p class="cormorant font-[600] xs:text-[16px] sm:text-[16px] lg:text-[24px] border-b leading-tight uppercase border-headingColor text-headingColor cursor-pointer hover:pb-[2px] hover:transition-all" id="position-btn" data-value="<?php echo get_sub_field('opened_position'); ?>">Apply</p>
             </div>
