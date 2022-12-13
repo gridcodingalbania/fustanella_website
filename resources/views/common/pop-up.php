b59e3cdc221d671564c8cc4ca7dcec2655b0cb26
orjanab <orjana@gridcoding.com>
2022-12-13 09:28:26 +0100

    remove text

--- resources/views/common/pop-up.php
+++ resources/views/common/pop-up.php
@@ -3,7 +3,7 @@
     <div class="thank-you-modal flex flex-col justify-center">
         <div class="flex flex-col items-center justify-center text-center 2xl:py-0 lg:py-0 md:py-[10%] sm:py-[10%] py-[15%] 2xl:px-0 lg:px-0 px-[10%]">
             <p class="cormorant text-egreen font-[600] 2xl:text-[26px] sm:text-[26px] text-[22px] uppercase">Thank you</p>
-            <p class="cormorant text-egreen font-[600] 2xl:text-[26px] sm:text-[26px] text-[22px] capitalize" id="pop-up-response-text">For your booking</p>
+            <p class="cormorant text-egreen font-[600] 2xl:text-[26px] sm:text-[26px] text-[22px] capitalize" id="pop-up-response-text"></p>
         </div>
     </div>
 </div>
