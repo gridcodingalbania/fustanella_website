24ea77ac5fa4d49b886a69aa384c241cdfbefe5d
greit1 <greit@gridcoding.com>
2022-12-27 09:52:53 +0100

    added animation on vacancies cards

--- resources/views/work-with-us/vacancies-cards.php
+++ resources/views/work-with-us/vacancies-cards.php
@@ -6,7 +6,7 @@
             $position_name = get_sub_field('opened_position');
         ?>
 
-            <div data-aos="fade-in" class="bg-[#F0F0F0] card-shadows flex flex-col items-center justify-around xs:w-[154px] sm:w-[180px] md:w-[300px] xl:w-[300px] 2xl:w-[384px] xs:h-[148px] sm:h-[148px] md:h-[200px] xl:h-[300px] 2xl:h-[384px] ">
+            <div class="bg-[#F0F0F0] card-shadows fade-animate flex flex-col items-center justify-around xs:w-[154px] sm:w-[180px] md:w-[300px] xl:w-[300px] 2xl:w-[384px] xs:h-[148px] sm:h-[148px] md:h-[200px] xl:h-[300px] 2xl:h-[384px] ">
                 <p class="cormorant font-[500] uppercase xs:text-[14px] sm:text-[14px] lg:text-[24px]"><?php the_sub_field('opened_position'); ?></p>
                 <?php if ($image) : ?>
                     <img src="<?php echo $picture ?>" class="xs:w-[38px] sm:w-[38px] lg:w-[52px] 2xl:w-auto">
